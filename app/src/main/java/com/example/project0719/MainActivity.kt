2688fb2e4882775c972b01f5daad295b3e6a1d48
Sagar Patil <sagarpatil@manish.mynewtonhq.com>
2019-07-02 18:02:43 +0530

    User home setup

--- app/src/main/java/com/example/project0719/MainActivity.kt
+++ app/src/main/java/com/example/project0719/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.project0719
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project0719.user.UserHome
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, UserHome::class.java))
+        finish()
     }
 }
